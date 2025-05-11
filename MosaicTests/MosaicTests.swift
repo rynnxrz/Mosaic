f3422d8d60cb49bbaa179055a4f5538176d13101
FanqieUoA <98016410+FanqieUoA@users.noreply.github.com>
2025-05-11 23:37:32 +0100

    Initial Commit

---
+++ MosaicTests/MosaicTests.swift
@@ -0,0 +1,17 @@
+//
+//  MosaicTests.swift
+//  MosaicTests
+//
+//  Created by Sheldon Xu on 11/05/2025.
+//
+
+import Testing
+@testable import Mosaic
+
+struct MosaicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
